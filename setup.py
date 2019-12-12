3c01c87c30b81ccc6180fa37c804d730df6b10e0
Tom Mertens <tomtommertens2@gmail.com>
2019-12-12 12:42:45 +0100

    added setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open('financejson/__about__.py') as file:
+with open('FinDataBroker/__about__.py') as file:
     about = {}
     exec(file.read(), about)
 
@@ -24,12 +24,13 @@ setup(
     test_requires=['pytest'],
     python_requires='>=3.6',
     include_package_data=True,
-    package_data={'financejson': ['schema.json'], },
-    entry_points={'console_scripts': ["financejson = financejson.cli:main"]},
+    # package_data={'financejson': ['schema.json'], },
+    # entry_points={'console_scripts': ["financejson = financejson.cli:main"]},
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
