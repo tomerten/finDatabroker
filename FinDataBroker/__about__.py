8d7916703c104ef87ba3ca9facae66518fbb3c70
Tom Mertens <tomtommertens2@gmail.com>
2019-12-17 21:47:43 +0100

    updated version

--- FinDataBroker/__about__.py
+++ FinDataBroker/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'FinDataBroker'
 __description__ = 'A databroker for financial data'
 __url__ = 'https://github.com/tomerten/findatabroker'
-__version__ = '1.0.0alpha2'
+__version__ = '1.0.0alpha3'
 __author__ = 'Tom Mertens'
 __license__ = 'GNU General Public License v3.0'
